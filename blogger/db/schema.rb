ed4f1d0afc229feb813d78f83937a0fa3b854eab
jibzoiderz <tapiador.jib@gmail.com>
2016-11-09 13:53:56 +0800

    I0 Added

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107065958) do
+ActiveRecord::Schema.define(version: 20161109050337) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
