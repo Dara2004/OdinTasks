9cbd2fe847510d023304dd2f9d852365c7bc9ed6
jibzoiderz <tapiador.jib@gmail.com>
2016-11-09 19:39:40 +0800

    Tagging feature complete

--- blogger/app/helpers/articles_helper.rb
+++ blogger/app/helpers/articles_helper.rb
@@ -4,4 +4,8 @@ module ArticlesHelper
         params.require(:article).permit(:title, :body)
     end
     
+    def article_params
+        params.require(:article).permit(:title, :body, :tag_list)
+    end
+
 end
