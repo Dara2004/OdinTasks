1a514e4647aed56479abc93a8c40a5da36484f64
jibzoiderz <tapiador.jib@gmail.com>
2016-11-20 23:12:56 +0800

    added adv

--- adv-building-blocks/bubbleSort.rb
+++ adv-building-blocks/bubbleSort.rb
@@ -2,6 +2,7 @@
 def bubble_sort(arr)
 	length = arr.length - 1
 	not_sorted = true
+
 	while not_sorted == true 
 		swapped = false
 
@@ -18,6 +19,7 @@ def bubble_sort(arr)
 end
 
 p bubble_sort([4,3,78,2,0,2])
+
 #2
 def bubble_sort_by(arr)
 	length = arr.length
@@ -25,20 +27,17 @@ def bubble_sort_by(arr)
 
 	while not_sorted == true 
 		swapped = false
-
 		(length - 1).times do |g|
 
 			if yield(arr[g],arr[g+1]) > 0		        
 		        arr[g], arr[g + 1] = arr[g + 1], arr[g]
-		        swapped = true
-		        
+		        swapped = true		        
 		    end
 		end
 		
 		not_sorted = false if swapped == false
 	end
 	return arr
-
 end
 
 p bubble_sort_by(["hi","hello","hey"]){ |left,right| left.length - right.length}
---
+++ adv-building-blocks/my_enurable_methods.rb
@@ -0,0 +1,85 @@
+module Enumerable
+
+	def my_each
+	    cntr = 0 
+		while self.size > cntr
+			yield(self[cntr])
+			cntr += 1
+		end
+
+		return self
+	end
+
+	def my_each_with_index
+		cntr = 0 
+		while self.size > cntr
+			yield(self[cntr],cntr)
+			cntr += 1
+		end
+
+		return self
+	end
+
+	def my_select
+		arr = Array.new
+		self.my_each{|elem| arr.push(elem) if yield(elem) }
+		return arr
+	end
+
+	def my_all? 
+		self.my_select{|elem| yield(elem)} == self ? true : false
+	end
+
+	def my_any?
+		self.my_select{|elem| yield(elem)}.size > 0 ? true : false
+	end
+
+	def my_none?
+		!my_any? {|element| yield (element) }
+	end
+
+	def my_count(acc = nil)
+		if acc 
+			return self.my_select {|elem| elem == acc}.size
+		end
+		if block_given?
+			return self.my_select {|elem| yield(elem)}.size
+		else
+			return my_count.size
+		end
+	end
+
+	def my_map(&block)
+		arr = Array.new
+		if block_given?
+			self.my_each do |elem|
+				arr << yield(element)
+			end
+		else
+			self.my_each do |elem|
+				arr << element.block.call
+			end
+		end
+	end
+
+	def my_inject(memo = nil)
+		if memo.nil?
+			memo = self[0]
+			i = 1
+		else
+			i = 0
+		end
+
+		while i < self.size
+			memo = yield(memo,self[i])
+			i += 1
+		end
+		return memo
+	end
+
+	def multiply_els(array)
+	    array.my_inject { |memo, element| memo * element }
+    end
+
+end
+
