b84664bbb79468b447a0e5af530b7e35b4955f7d
jibzoiderz <tapiador.jib@gmail.com>
2016-11-28 13:08:54 +0800

    fixed some bugs thx to @105ron

--- TicTacToe/game.rb
+++ TicTacToe/game.rb
@@ -6,16 +6,14 @@ game = TicTacToe.new
 puts "Please follow the default layout\n
 Press a number to play"
 game.default_layout 
-puts game.win?
-puts game.tied?
 
-while game.win? != false || game.tied? != false
+until game.win? || game.tied? 
 	posit = gets.chomp
 	game.add_position(posit.to_i)
 	game.output_board
 	puts " "
-	puts game.win?
-    puts game.tied?
+
 end
 
-
+puts "A player has won the game" if game.win?
+puts "It is a tie" if game.tied?
